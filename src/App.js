8d5a221f117be373fa2737ee6961613f04dff774
Marinela <nelamarinela17@gmail.com>
2023-09-01 13:40:14 +0200

    Routing Practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 
 function App() {
+  function Home() {
+    return <h1>Welcome</h1>;
+  }
+
+  function Element() {
+    const { element } = useParams();
+
+    if(isNaN(element)) {
+      return ShowText(element)
+    } else {
+      return Number(element);
+    }
+  }
+
+  function Number(number) {
+    return <h1>The number is: {number}</h1>
+  }
+
+  function ShowText(text) {
+    return <h1>The text is: {text}</h1>
+  }
+
+  function ChangeColor() {
+     const { word, color, background } = useParams();
+    return <h1 style={{color: color, backgroundColor: background}}>The word is: {word}</h1>
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/:element" element={<Element />}></Route>
+          <Route path="/:word/:color/:background" element={<ChangeColor />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
